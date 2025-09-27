cb6ed26b4529da08220151ae912a016a602640c6
Nguyenthithuhang0406 <hangthongminh04062004@gmail.com>
2025-09-27 19:57:57 +0700

    manager student

---
+++ app/src/main/java/com/example/managerstudent/MainActivity.java
@@ -0,0 +1,189 @@
+package com.example.managerstudent;
+
+import android.app.DatePickerDialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.Toast;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText edtName, edtBirthday;
+    private Button btnSelectdate, btnNew, btnUpdate, btnRemove, btnClose;
+    private RadioButton rdMale, rdfemale;
+    private ListView lvStudents;
+    private List<Student> arrStudents;
+    private ArrayAdapter arrayAdapter;
+    private int pos;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        mapping();
+        arrStudents = new ArrayList<Student>();
+        arrStudents.add(new Student("Le la", "15/02/2003", true));
+        arrStudents.add(new Student("Le la2", "15/02/2003", false));
+        arrStudents.add(new Student("Le la3", "15/02/2003", true));
+        arrayAdapter = new ArrayAdapter(MainActivity.this, android.R.layout.simple_list_item_1, arrStudents);
+        lvStudents.setAdapter(arrayAdapter);
+        selectedStudent(0);
+
+       lvStudents.setOnItemClickListener((parent, view, position, id) -> {
+              pos = position;
+              selectedStudent(pos);
+       });
+
+        btnSelectdate.setOnClickListener(v -> {
+            selectDate();
+        });
+
+        btnRemove.setOnClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("Chu y");
+            builder.setMessage("Ban co muon xoa khong?");
+            builder.setPositiveButton("Xoa", (dialog, which) -> {
+                arrStudents.remove(pos);
+                arrayAdapter.notifyDataSetChanged();
+                //xoa noi dung ban ghi bi xoa tren cac view
+                selectedStudent(-1);
+                if (arrStudents.isEmpty()) {
+                    btnRemove.setEnabled(false);
+                }
+            });
+            builder.setNegativeButton("Huy", null);
+            builder.show();
+        });
+
+        btnNew.setOnClickListener(v -> {
+            if (btnNew.getText().toString().equals("New")) {
+                btnNew.setText("Save");
+                btnRemove.setEnabled(false);
+                btnUpdate.setEnabled(false);
+                selectedStudent(-1);
+            } else {
+                Student student = getStudent();
+                arrStudents.add(student);
+                arrayAdapter.notifyDataSetChanged();
+                btnNew.setText("New");
+                btnRemove.setEnabled(true);
+                btnUpdate.setEnabled(true);
+                Toast.makeText(MainActivity.this, "Da them thanh cong", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        btnUpdate.setOnClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("chu y");
+            builder.setMessage("Co chac chan muon cap nhat khong?");
+            builder.setPositiveButton("Cap nhat", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    arrStudents.set(pos, getStudent());
+                    arrayAdapter.notifyDataSetChanged();
+                }
+            });
+            builder.setNegativeButton("Huy", null);
+            builder.show();
+        });
+
+        btnClose.setOnClickListener(v -> {
+            AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+            builder.setTitle("chu y");
+            builder.setMessage("Co chac chan muon dong ung dung khong?");
+            builder.setPositiveButton("Dong", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    finish();
+                }
+            });
+            builder.setNegativeButton("Huy", null);
+            builder.show();
+        });
+    }
+
+    private Student getStudent() {
+        Student student = new Student();
+        student.setName(edtName.getText().toString());
+        student.setBirthday(edtBirthday.getText().toString());
+        if (rdMale.isChecked()) {
+            student.setSex(true);
+        } else {
+            student.setSex(false);
+        }
+        return student;
+    }
+
+    private void selectDate() {
+        //lay ngay hien tai
+        final Calendar calendar = Calendar.getInstance();
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        //Tao DatePickerDialog
+        DatePickerDialog datePickerDialog = new DatePickerDialog(MainActivity.this, new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int y, int m, int d) {
+                String date = d + "/" + (m + 1) + "/" + y;
+                edtBirthday.setText(date);
+            }
+        }, year, month, day);
+        datePickerDialog.show();
+
+    }
+
+    private void selectedStudent(int pos) {
+        if (pos >= 0) {
+            Student student = arrStudents.get(pos);
+            edtName.setText(student.getName());
+            edtBirthday.setText(student.getBirthday());
+            if (student.isSex()) {
+                rdMale.setChecked(true);
+            } else {
+                rdfemale.setChecked(true);
+            }
+        } else {
+            edtName.setText("");
+            edtBirthday.setText("");
+            rdMale.setChecked(false);
+            rdfemale.setChecked(false);
+        }
+    }
+
+    private void mapping() {
+        edtName = findViewById(R.id.edt_name);
+        edtBirthday = findViewById(R.id.edt_birthday);
+        rdMale = findViewById(R.id.rd_male);
+        rdfemale = findViewById(R.id.rd_female);
+        lvStudents = findViewById(R.id.lv_students);
+        btnSelectdate = findViewById(R.id.btn_select_date);
+        btnNew = findViewById(R.id.btn_new);
+        btnUpdate = findViewById(R.id.btn_update);
+        btnRemove = findViewById(R.id.btn_remove);
+        btnClose = findViewById(R.id.btn_close);
+    }
+}
---
+++ app/src/main/java/com/example/managerstudent/Student.java
@@ -0,0 +1,55 @@
+package com.example.managerstudent;
+
+public class Student {
+    private String name;
+    private String birthday;
+    private boolean sex;
+
+    @Override
+    public String toString() {
+        String gt = "";
+        if (this.sex) {
+            gt = "Nam";
+        } else {
+            gt = "Nu";
+        }
+        return "Student{" +
+                "Student: '" + name + '\'' +
+                ", '" + birthday + '\'' +
+                ", " + gt +
+                '}';
+    }
+
+    public Student() {
+    }
+
+    public Student(String name, String birthday, boolean sex) {
+        this.name = name;
+        this.birthday = birthday;
+        this.sex = sex;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public boolean isSex() {
+        return sex;
+    }
+
+    public void setSex(boolean sex) {
+        this.sex = sex;
+    }
+}
